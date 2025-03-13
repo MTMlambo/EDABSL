76d27a0df64d4ca61d90b4802ed290d12040179f
Michelle Mlambo <mlambomichelle2@gmail.com>
2025-03-13 08:53:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
+
+
+st.title("My first App")
+st.header("Module is EDAB")
+st.subheader("Its a fun module")
 
 # Generate random time series data
 time_series = np.random.randn(100)
 
-# Plot the time series
-plt.plot(time_series)
-plt.title("Random 100-Unit Time Series")
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.show()
+variable = st.button("My button")
+if variable
+  time_series = np.random.randn(2000)
+  fig, ax = plt.subplots()
+  ax.plot(time_series)
+  ax.set_title("my graph")
+  ax.set_xlabel("units")
+  ax.set_ylabel("value")
+  st.pyplot(fig)
+
+
